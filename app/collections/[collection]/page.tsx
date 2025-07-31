3e77ab3d4b30f6deb578a87e7fa36dfd1abac212
liho <liho@revenuemonster.my>
2025-07-31 14:51:59 +0800

    fix: add padding to collection description for improved readability

--- app/collections/[collection]/page.tsx
+++ app/collections/[collection]/page.tsx
@@ -78,7 +78,7 @@ export default async function CategoryPage({
                 </div>
                 <div className="mr-auto w-auto text-2xl">
                   {collection?.description && (
-                    <p className="text-sm text-gray-600 dark:text-gray-400">
+                    <p className="text-sm text-gray-600 dark:text-gray-400 pr-5 pb-5">
                       {collection.description}
                     </p>
                   )}
